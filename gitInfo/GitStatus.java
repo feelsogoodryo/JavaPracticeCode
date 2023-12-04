9333dcdf6cda9c6fec841805d68e0db8ca516d57
feelsogoodryo2 <ryo.ohama0131@gmail.com>
2023-12-04 16:16:10 +0900

    gitInfo push

---
+++ gitInfo/GitStatus.java
@@ -0,0 +1,24 @@
+package gitInfo;
+
+public class GitStatus {
+	/*
+	* untracked
+	 * ワーキングディレクトリに、ステージングエリアで登録されていないファイルがある状態。
+    * modified
+	 * すでにGit管理されているファイルが、ワーキングディレクトリ、もしくは、ステージングエリアで変更されている状態。
+	* new file
+	 * ステージングエリアの中に、まだコミットされていない新しいファイルがある状態。
+	 * 
+	 * git add
+	 * git commit -m"text"
+	 * git push
+	 * 
+	 * 
+	 * git status
+	 * git log
+	 * git restore ファイル名    //ワーキングエリアに加えた変更を、ステージングエリアの状態に戻したい場合
+	 * git restore --staged ファイル名    //ステージングエリアに加えた変更を、ローカルリポジトリの状態に戻したい場合	
+	 * 
+	 *  
+	 * */
+}
