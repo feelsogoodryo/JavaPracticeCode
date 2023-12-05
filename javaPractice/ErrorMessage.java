cac3101517b660d732cf1e399753c3c0304260be
feelsogoodryo2 <ryo.ohama0131@gmail.com>
2023-12-05 11:13:25 +0900

    addErrorMessegeSummary

---
+++ javaPractice/ErrorMessage.java
@@ -0,0 +1,40 @@
+package javaPractice;
+
+public class ErrorMessage {
+	/*
+	 *  エラーメッセージ 〜に適切なコンストラクタが見つかりません (引数がありません）
+	 *  〜は〜で private アクセスされます 
+	 *  〜は不可視です 
+	 *  〜を〜に解決できません 
+	 *  ¥12288は不正な文字です 
+	 *  ”がありません 
+	 *  class、interface、または enumがありません がありません 
+	 *  <identiFier>がありません
+	 *  java.lang.ArithmeticException: / by zero 
+	 *  java.lang.ArrayIndexOutOfBoundsException 
+	 *  java.lang.ClassCastException 
+	 *  java.lang.NoClassDefFoundError: ~ 
+	 *  java.lang. NoSuchMethodError: main 
+	 *  java.lang.NullPointerException 
+	 *  javacは、内部コマンドまたは外部コマンド、操作可能なプログラムまたはバッチファイルとして認識されていません 
+	 *  public 型〜はそれ独自のファイル内に定義されなければなりません 
+	 *  return 文が指定されていません 
+	 *  static でないメソッド〜を static コンテキストから参照することはできません 
+	 *  「暗黙的スーパー・コンストラクタ～は、デフォルト・コンストラクタについて は未定義です 
+	 *  クラス～はpublic であり、ファイル〜で宣言する必要があります 
+	 *  クラス名「'Main'が受け入れられるのは、注釈処理が明示的にリクエストされた場合のみです 
+	 *  構文解析中にファイルの終わりに移りました 
+	 *  互換性のない型 
+	 *  このメソッドは型〜の結果を返す必要があります 
+	 *  処理されない例外の型〜 
+	 *  シンボルを見つけられません 
+	 *  非static メソッド～を static 参照できません 
+	 *  変数〜は初期化されていない可能性があります 
+	 *  メイン・メソッドがクラス Main で見つかりません 
+	 *  メソッド～はすでにクラス〜で定義されています 
+	 *  例外～は報告されません
+	 * 
+	 * 
+	 * */
+
+}
