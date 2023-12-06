b25ef4821518b7383863a07073679a9a4b484eea
feelsogoodryo2 <ryo.ohama0131@gmail.com>
2023-12-06 09:40:41 +0900

    dokoJava2-1 finish

---
+++ dokoJavaPractice/TitleComparator.java
@@ -0,0 +1,9 @@
+package dokoJavaPractice;
+
+import java.util.Comparator;
+
+public class TitleComparator implements Comparator<Book> {
+	  public int compare(Book x, Book y) {
+		    return x.getTitle().compareTo(y.getTitle());
+	 }
+}
