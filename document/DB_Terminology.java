2b5bef861107e3a0e94c90e76bd0e9ac9b358647
feelsogoodryo2 <ryo.ohama0131@gmail.com>
2023-12-12 22:14:24 +0900

    alter document dir

---
+++ document/DB_Terminology.java
@@ -0,0 +1,24 @@
+package document;
+
+public class DB_Terminology {
+/*
+ * ★H2dbについて
+ * 組み込みモード：DBを一つのJavaアプリのみから開ける。
+ * サーバーモード：DBを複数のJavaアプリから開ける。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
+}
---
+++ document/IT_Terminology.java
@@ -0,0 +1,17 @@
+package document;
+
+public class IT_Terminology {
+	/* 
+	 * IT用語
+	 * 
+	 * プレースホルダ：仮で入っている情報
+	 * String.format(%)←％から始まるこれもプレースホルダーと呼ばれる。
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
+
+}
